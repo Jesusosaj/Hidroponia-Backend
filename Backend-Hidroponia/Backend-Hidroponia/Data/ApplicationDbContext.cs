37c0eeb008381acd788ef3a19e10bb92f6015b87
IvanMendez-ux <fabianivan56@gmail.com>
2025-03-22 17:47:35 -0300

    api back

---
+++ Backend-Hidroponia/Backend-Hidroponia/Data/ApplicationDbContext.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using Backend_Hidroponia.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend_Hidroponia.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Estado> Estados { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=hidroponia_db;Username=postgres;Password=1234");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasPostgresExtension("pgcrypto");
+
+        modelBuilder.Entity<Estado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("estados_pkey");
+
+            entity.ToTable("estados");
+
+            entity.HasIndex(e => e.Nombre, "estados_nombre_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("roles_pkey");
+
+            entity.ToTable("roles");
+
+            entity.HasIndex(e => e.Nombre, "roles_nombre_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Descripcion).HasColumnName("descripcion");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .HasColumnName("nombre");
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("usuarios_pkey");
+
+            entity.ToTable("usuarios");
+
+            entity.HasIndex(e => e.Correo, "idx_usuarios_correo");
+
+            entity.HasIndex(e => e.NombreUsuario, "idx_usuarios_nombre_usuario");
+
+            entity.HasIndex(e => e.Correo, "usuarios_correo_key").IsUnique();
+
+            entity.HasIndex(e => e.NombreUsuario, "usuarios_nombre_usuario_key").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.ActualizadoEn)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("actualizado_en");
+            entity.Property(e => e.Contrasena).HasColumnName("contrasena");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(100)
+                .HasColumnName("correo");
+            entity.Property(e => e.CreadoEn)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("creado_en");
+            entity.Property(e => e.EstadoId).HasColumnName("estado_id");
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(50)
+                .HasColumnName("nombre_usuario");
+            entity.Property(e => e.RolId).HasColumnName("rol_id");
+
+            entity.HasOne(d => d.Estado).WithMany(p => p.Usuarios)
+                .HasForeignKey(d => d.EstadoId)
+                .HasConstraintName("usuarios_estado_id_fkey");
+
+            entity.HasOne(d => d.Rol).WithMany(p => p.Usuarios)
+                .HasForeignKey(d => d.RolId)
+                .HasConstraintName("usuarios_rol_id_fkey");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Backend-Hidroponia/Backend-Hidroponia/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Backend_Hidroponia.Models;
+
+public partial class Usuario
+{
+    public int Id { get; set; }
+
+    public string NombreUsuario { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Contrasena { get; set; } = null!;
+
+    public int RolId { get; set; }
+
+    public int EstadoId { get; set; }
+
+    public DateTime? CreadoEn { get; set; }
+
+    public DateTime? ActualizadoEn { get; set; }
+
+    public virtual Estado Estado { get; set; } = null!;
+
+    public virtual Role Rol { get; set; } = null!;
+}
